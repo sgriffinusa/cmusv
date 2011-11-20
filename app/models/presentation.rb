4abcb441a2050e9020badfecae64341d17d52422
Andrew Chen <andrew.chen@sv.cmu.edu>
2011-11-20 13:16:56 -0800

    Add presentation model validation

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -1,2 +1,36 @@
+# The presentation model represents a presentation given by a team or individual.
+#
+# A presentation has the following fields:
+#   [course]          The course that a presentation belongs to. Can be null.
+#   [team]            The team that is presenting, if it is a team presentation.
+#   [individual]      The individual that is presenting, if it is an individual presentation.
+#   [creator]         The person that created the presentation instance.
+#   [title]           The title of the presentation.
+#   [comments]        Description of the presentation, if any.
 class Presentation < ActiveRecord::Base
+  belongs_to :creator, :class_name=>'User', :foreign_key => 'creator_user_id', :validate => true
+  belongs_to :team, :validate => true
+  belongs_to :individual, :class_name=>'User', :foreign_key => 'individual_user_id', :validate => true
+
+  validates_presence_of :creator
+
+  validate :team_or_individual, :creator_is_faculty
+
+  protected
+  # A presentation has to be for either a team or individual, but not both
+  def team_or_individual
+    if (team_id && individual_user_id)
+      errors.add(:team_id, "A presentation can't belong to both a team and individual")
+    elsif (!team_id && !individual_user_id)
+      errors.add(:team_id, "A team or individual must be specified")
+    end
+  end
+
+  # Only faculty or admin are allowed to create presentations
+  def creator_is_faculty
+    if (creator && !(creator.is_teacher? || creator.is_admin?))
+        errors.add(:creator, "Must be faculty or admin to create a presentation")
+    end
+  end
+
 end
--- spec/models/presentation_spec.rb
+++ spec/models/presentation_spec.rb
@@ -1,5 +1,62 @@
 require 'spec_helper'
 
 describe Presentation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @faculty_frank = Factory(:faculty_frank)
+    @student_sam = Factory(:student_sam)
+    @student_sally = Factory(:student_sally)
+    @admin_andy = Factory(:admin_andy)
+    @course = Factory(:mfse_current_semester, :configure_teams_name_themselves => false)
+    @team_triumphant = Factory(:team, :course_id => @course.id, :people => [@student_sam, @student_sally])
+
+    @valid_attributes = {
+        :course_id => @course.id,
+        :team_id => @team_triumphant.id,
+        :individual_user_id => nil,
+        :creator_user_id => @faculty_frank.id,
+        :title => "Task 1 Presentation",
+        :comments => "This is a presentation for Task 1"
+    }
+  end
+
+  it "should create new presentation with valid attributes" do
+    lambda {Presentation.create!(@valid_attributes)}.should_not raise_error
+  end
+
+  it "should not allow both team and individual to be specified" do
+    @valid_attributes[:team_id] = @team_triumphant.id
+    @valid_attributes[:individual_user_id] = @student_sam.id
+    presentation = Presentation.create(@valid_attributes)
+    presentation.should have(1).errors_on(:team_id)
+  end
+
+  it "should require either a team or individual to be specified" do
+    @valid_attributes[:team_id] = nil
+    @valid_attributes[:individual_user_id] = nil
+    presentation = Presentation.create(@valid_attributes)
+    presentation.should have(1).errors_on(:team_id)
+  end
+
+  it "should require a valid creator" do
+    @valid_attributes[:creator_user_id] = nil
+    lambda {Presentation.create!(@valid_attributes)}.should raise_error(ActiveRecord::RecordInvalid)
+
+    @valid_attributes[:creator_user_id] = 123456789000
+    lambda {Presentation.create!(@valid_attributes)}.should raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "should only allow faculty or admin to create a presentation" do
+    @valid_attributes[:creator_user_id] = @student_sam.id
+    presentation = Presentation.create(@valid_attributes)
+    presentation.should have(1).errors_on(:creator)
+
+    @valid_attributes[:creator_user_id] = @faculty_frank.id
+        presentation = Presentation.create(@valid_attributes)
+    presentation.should have(0).errors_on(:creator)
+
+        @valid_attributes[:creator_user_id] = @admin_andy.id
+        presentation = Presentation.create(@valid_attributes)
+    presentation.should have(0).errors_on(:creator)
+  end
+
 end
