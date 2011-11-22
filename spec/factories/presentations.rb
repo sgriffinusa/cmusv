1a3093cebb6faa0d5aa98600a1395172262abd0f
Hongdi Li <hongdi.li@sv.cmu.edu>
2011-11-22 13:43:40 -0800

    add course to the seed data for presentations.

--- spec/factories/presentations.rb
+++ spec/factories/presentations.rb
@@ -1,6 +1,7 @@
 require File.join(Rails.root,'spec','factories','factories.rb')
 
 Factory.define :presentation_team, :parent => :presentation do |p|
+  p.association :course, :factory => :course
   p.association :team, :factory => :team_triumphant
   p.individual_user_id nil
   p.association :creator, :factory => :faculty_frank
@@ -8,10 +9,12 @@ Factory.define :presentation_team, :parent => :presentation do |p|
   p.comments "Give a 20min presentation. Requirements: 1,2,3,4,5,6,9"
 end
 
+
 Factory.define :presentation_individual, :parent => :presentation do |p|
+  p.course_id nil
   p.team_id nil
-  p.association :individual, :factory => :student_sam
-  p.association :creator, :factory => :faculty_frank
+  p.association :individual, :factory => :student_sally
+  p.association :creator, :factory => :faculty_fagan
   p.title "Individual Task 2 Presentation"
   p.comments "Give a 5min presentation. Requirements: 1,2,3,4,5,6,9"
 end
