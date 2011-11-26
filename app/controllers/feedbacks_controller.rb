55e767fed0929a30cc3816887e75e25b776156ba
Hongdi Li <hongdi.li@sv.cmu.edu>
2011-11-26 00:24:17 -0800

    Add show action to feedback controller

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,5 +1,19 @@
 class FeedbacksController < ApplicationController
+
+  before_filter :authenticate_user!
+
+  layout 'cmu_sv_no_pad'
+
+
+  # GET /feedbacks/1
+  # GET /feedbacks/1.xml
   def show
+    @feedback = Feedback.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml { render :xml => @feedback }
+    end
   end
 
   def new
